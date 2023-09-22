ff3b539f14d5f8441fa7ee02cd249d2436880a94
jpedro002 <joaopsilvavolei@gmail.com>
2023-09-22 10:24:05 -0300

    config prismic

--- src/services/prismic.js
+++ src/services/prismic.js
@@ -1,8 +1,8 @@
 import * as prismic from '@prismicio/client'
 
 // Fill in your repository name
-export const repositoryName = 'jotaportf'
+export const repositoryName = ''
 
 export const client = prismic.createClient(repositoryName, {
-  accessToken: 'MC5aUXl6V3hBQUFDa0FRWWpN.77-977-9XUXvv73vv73vv73vv70fNe-_ve-_vQsG77-9H--_vQtdHu-_ve-_vX1qQw1R77-9T--_vRrvv70',
+  accessToken: '',
 })
