d137b71d62e7d9b930a4bb37a675b406b3c1bdb9
jpedro002 <joaopsilvavolei@gmail.com>
2023-09-23 15:59:51 -0300

    fix: prismic env

--- src/services/prismic.js
+++ src/services/prismic.js
@@ -1,9 +1,5 @@
-import 'dotenv/config'
-import * as prismic from '@prismicio/client'
+import * as Prismic from '@prismicio/client';
 
-
-export const client = prismic.createClient(process.env.REPOSITORY_NAME, {
-  accessToken: process.env.ACCESS_TOKEN,
-})
-
-console.log(client);
+export const client = Prismic.createClient(`${import.meta.env.VITE_PRISMIC_ENVIRONMENT}`, {
+  accessToken: `${import.meta.env.VITE_PRISMIC_ACCESS_TOKEN}`,
+});
